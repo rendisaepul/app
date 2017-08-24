ce1f3255525494ac707253523c2180996515758d
Rizal Sidik Permana <rizalsidikp24@gmail.com>
2017-08-24 22:18:06 +0700

    #95 add transfer click function press

--- app/components/GrantPointCard/index.js
+++ app/components/GrantPointCard/index.js
@@ -18,7 +18,10 @@ import styles from './styles';
 class GrantPointCard extends Component {
   constructor(props) {
     super(props);
-    this.state = { isOpen: false }
+    this.state = {
+      isOpen: false,
+      addPoints: ''
+    }
   }
 
   handleToggle = () => {
@@ -26,16 +29,13 @@ class GrantPointCard extends Component {
     this.setState({ isOpen });
   }
 
+  handleChange = (value) => {
+    this.setState({ addPoints: value })
+  }
+
   render() {
     const { isOpen } = this.state;
-    const { title, description, stage, time_start, time_end } = this.props;
-    const start = new moment(time_start);
-    const end = new moment(time_end);
-    const dayStart = start.format('DD');
-    const monthStart = start.format('MMM');
-    const timeStart = start.format('hh:mm A');
-    const dateEnd = end.format('MMM DD');
-    const timeEnd = end.format('hh:mm A');
+    const { firstName, lastName, username, points, id } = this.props;
     return (
       <Card style={styles.container}>
         <TouchableHighlight onPress={()=> this.handleToggle() }>
@@ -45,9 +45,9 @@ class GrantPointCard extends Component {
               source={{uri: 'https://s-media-cache-ak0.pinimg.com/736x/bc/f0/4e/bcf04eafebdf707b8d900f02e6d8bd70--photo-tag-touch-me.jpg'}}
             />
             <View>
-              <Text style={styles.eventTitle}> Firstname Lastname </Text>
+              <Text style={styles.eventTitle}>{firstName} {lastName}</Text>
               <Text style={{ marginLeft: 5 }}>
-                <Text style={[styles.smallText]}>CEO Of Google</Text>
+                <Text style={[styles.smallText]}>{username}</Text>
               </Text>
             </View>
             <Right>
@@ -60,13 +60,13 @@ class GrantPointCard extends Component {
             <CardItem>
               <View style={{ flex: 1 }} >
                 <Text style={{ fontWeight: 'bold', marginBottom: 10, marginLeft: 5 }} >
-                  Point : 100
+                  Point : {points}
                 </Text>
                 <View style={{ flex: 1, flexDirection: 'row' }} >
                   <Item regular style={{ flex: 1, height: 40 }} >
-                    <Input placeholder='Add Point' keyboardType='numeric' style={{ fontSize: 14 }} />
+                    <Input placeholder='Add Point' keyboardType='numeric' style={{ fontSize: 14 }} value={this.state.addPoints} onChangeText={this.handleChange} />
                   </Item>
-                  <Button style={{ height: 40, borderRadius: 0, borderWidth: 0 }} onPress={() => console.log('ini di click')} >
+                  <Button style={{ height: 40, borderRadius: 0, borderWidth: 0 }} onPress={() => this.props.onClickTransfer(id, this.state.addPoints)} >
                     <Text>Transfer</Text>
                   </Button>
                 </View>
