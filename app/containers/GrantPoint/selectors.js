2726e79d6fe2b0171375ce2c318398dfc8a8ef64
Rizal Sidik Permana <rizalsidikp24@gmail.com>
2017-08-24 19:36:49 +0700

    #95 add grant point selectors

---
+++ app/containers/GrantPoint/selectors.js
@@ -0,0 +1,28 @@
+/**
+ *  Import node modules
+ */
+import { createSelector } from 'reselect';
+
+/**
+ *  Select the main portion of the root reducer
+ */
+const selectGrantPointReducer = () => state => state.get('grantPoint');
+
+/**
+ *  Selects the username field data
+ *  Returns object
+ */
+export const getAttendees = () => createSelector(
+  selectGrantPointReducer(),
+  state => state.get('attendeesData').toJS()
+);
+
+/**
+ *  Get logged in status
+ *  Returns boolean
+ */
+export const getIsGettingAttendees = () => createSelector(
+  selectGrantPointReducer(),
+  state => state.get('isGettingAttendees')
+);
+
