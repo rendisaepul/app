a52b669eca6baabf4a392dbbecbba25704a2dce7
Rizal Sidik Permana <rizalsidikp24@gmail.com>
2017-08-24 22:18:49 +0700

    #95 add transfer function constants

--- app/containers/GrantPoint/constants.js
+++ app/containers/GrantPoint/constants.js
@@ -1,2 +1,3 @@
 export const GET_ATTENDEES = 'app/containers/GrantPoint/GET_ATTENDEES';
 export const IS_GETTING_ATTENDEES = 'app/containers/GrantPoint/IS_GETTING_ATTENDEES';
+export const IS_TRANSFERING_POINTS = 'app/containers/GrantPoint/IS_TRANSFERING_POINTS';
