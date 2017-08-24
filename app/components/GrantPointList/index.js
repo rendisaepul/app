67ec963d20437e285f5577012f051e96b22cebb1
Rizal Sidik Permana <rizalsidikp24@gmail.com>
2017-08-24 22:17:37 +0700

    #95 add transfer click function

--- app/components/GrantPointList/index.js
+++ app/components/GrantPointList/index.js
@@ -11,11 +11,12 @@ class GrantPointList extends Component{
         { events.length > 0 && events.map((event, index)=> (
           <GrantPointCard
             key={index}
-            title={event.title}
-            description={event.description}
-            stage={event.stage}
-            time_start={event.time_start}
-            time_end={event.time_end}
+            firstName={event.user.first_name}
+            lastName={event.user.last_name}
+            username={event.user.username}
+            points={event.points}
+            onClickTransfer={this.props.onClickTransfer}
+            id={event.id}
           />
         ))}
       </ScrollView>
